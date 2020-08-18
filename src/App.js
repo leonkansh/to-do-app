b4d94b09a882e03c1d9d8c603ed12676a295c7d7
Leon Kan <leon.can@outlook.com>
2020-08-18 10:38:47 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ToDo from "./ToDo"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToDo/>
     </div>
   );
 }
 
 export default App;
+
+function TodosComponent() {
+  const [currentTodo, setCurrentTodo] = useState("");
+  const [todos, setTodos] = useState([
+    // state as examples
+    {
+      todo: "bake a cake",
+      isCompleted: true
+    },
+    {
+      todo: "go for a walk",
+      isCompleted: false
+    },
+    {
+      todo: "contribute a web development tutorial on Enlight",
+      isCompleted: false
+    }
+  ]);
+
+  return (
+    <div>
+      {/* <h1>hellow from our Todo component!</h1> */}
+      {todos.map((todo, index) => (
+        <p>{todo.todo}</p>
+      ))}
+      {todos.length > 0 && `${todos.length} items`}
+    </div>
+  );
+}
+
+export default TodosComponent;
